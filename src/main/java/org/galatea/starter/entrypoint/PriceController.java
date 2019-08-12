199b09b0c2c55b2bc4960ddd698579e2c6a6ed86
James Reedie <jreedie@Galatea.com>
2019-08-12 09:26:06 -0400

    fixed up issue 1, completed issue 2

---
+++ src/main/java/org/galatea/starter/entrypoint/PriceController.java
@@ -0,0 +1,23 @@
+package org.galatea.starter.entrypoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PriceController extends BaseRestController {
+
+  protected void processRequestId(final String requestId) {
+    return;
+  }
+  /**
+   * Receives and responds to requests for yahoo finance stock information.
+   */
+
+  @RequestMapping("/prices")
+  public String hello(@RequestParam(value = "stock") final String stock,
+      @RequestParam(value = "numDays") final String numDays) {
+    return stock + ", " + numDays;
+  }
+
+}
